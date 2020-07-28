04336566cc0e5829844e7ad367f9ea5bf640e31c
pankajraj01 <pankajrajpurohit01@gmail.com>
2020-07-28 15:27:56 +0530

    StaggeredRecyclerView Implemented sucessfully.

---
+++ app/src/main/java/com/example/androidgittesting/SteggeredRecyclerViewAdapter.java
@@ -0,0 +1,82 @@
+package com.example.androidgittesting;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+public class SteggeredRecyclerViewAdapter extends RecyclerView.Adapter<SteggeredRecyclerViewAdapter.ViewHolder> {
+
+    private static final String TAG = "SteggeredRecyclerViewAdapter";
+
+    private Context mContext;
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImages = new ArrayList<>();
+
+    public SteggeredRecyclerViewAdapter(Context mContext, ArrayList<String> mNames, ArrayList<String> mImages) {
+        this.mContext = mContext;
+        this.mNames = mNames;
+        this.mImages = mImages;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_grid_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+
+        Log.d(TAG, "onBindViewHolder: called.");
+
+        RequestOptions requestOptions = new RequestOptions()
+                .placeholder(R.drawable.ic_launcher_background);
+
+        Glide.with(mContext)
+                .load(mImages.get(position))
+                .apply(requestOptions)
+                .into(holder.image);
+
+        holder.name.setText(mNames.get(position));
+
+        holder.image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked on" + mNames.get(position));
+                Toast.makeText(mContext, mNames.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImages.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView image;
+        TextView name;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.image = itemView.findViewById(R.id.image_view_widget);
+            this.name = itemView.findViewById(R.id.name_widget);
+        }
+    }
+}
+
