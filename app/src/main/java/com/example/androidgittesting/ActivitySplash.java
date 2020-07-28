12c7ebc72a674d25e377b915ceb432dd0ad87f5f
pankajraj01 <pankajrajpurohit01@gmail.com>
2020-07-28 17:55:59 +0530

    Splash Activity Added...

---
+++ app/src/main/java/com/example/androidgittesting/ActivitySplash.java
@@ -0,0 +1,26 @@
+package com.example.androidgittesting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class ActivitySplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(ActivitySplash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+
+    }
+}
